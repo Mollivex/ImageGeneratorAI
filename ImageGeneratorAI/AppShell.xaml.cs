0f88f49f493b25df3bdfbdde61e3e8a28baa5cab
Alexandr Cocier <Limos12345@outlook.com>
2023-11-08 22:26:59 +0200

    Add project files.

---
+++ ImageGeneratorAI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ImageGeneratorAI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ImageGeneratorAI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ImageGeneratorAI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
