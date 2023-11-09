3eed67542a00dfa6c872713a29b71b01d74cb563
Alexandr Cocier <Limos12345@outlook.com>
2023-11-09 08:33:42 +0200

    Creating Dashboard Panel

--- ImageGeneratorAI/App.xaml.cs
+++ ImageGeneratorAI/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace ImageGeneratorAI
+﻿using ImageGeneratorAI.Views;
+
+namespace ImageGeneratorAI
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new DashboardView();
         }
     }
 }
