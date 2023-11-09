ba13c62e2df1b2e57f6dbfd7f044299c856c8bf0
Alexandr Cocier <Limos12345@outlook.com>
2023-11-09 21:15:39 +0200

    Add Animation Image Processing Generation

--- ImageGeneratorAI/Views/ImageGeneratorView.xaml.cs
+++ ImageGeneratorAI/Views/ImageGeneratorView.xaml.cs
@@ -12,10 +12,24 @@ public partial class ImageGeneratorView : ContentPage
 		StopGeneration();
     }
 
-	private void StopGeneration()
+	private async void StopGeneration()
 	{
+		await Task.Delay(2000);
+
 		lottie.IsAnimationEnabled = false;
 		lottie.IsVisible = false;
 		imageBorder.IsVisible = true;
+		imageAnimation.IsVisible = true;
+
+		await Task.WhenAny(
+			imageAnimation.ScaleTo(1.1, 1000),
+			imageAnimation.FadeTo(0, 1000),
+			borderTime.ScaleTo(1, 1000),
+			borderTime.FadeTo(1, 1000)
+			);
+
+		await borderTime.FadeTo(0, 300);
+		await btnFinish.ScaleTo(1, 1000);
+
 	}
 }
