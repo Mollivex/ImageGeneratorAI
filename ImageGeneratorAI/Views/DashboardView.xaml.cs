16965ca5ff18d3e3643755fe670fcf6aa8366ebb
Alexandr Cocier <Limos12345@outlook.com>
2023-11-09 10:14:53 +0200

    Creating Profiles Dashboard Panels

--- ImageGeneratorAI/Views/DashboardView.xaml.cs
+++ ImageGeneratorAI/Views/DashboardView.xaml.cs
@@ -17,6 +17,62 @@ public partial class DashboardView : ContentPage
 
 	private void LoadData()
 	{
-		throw new NotImplementedException();
+		Profiles = new ObservableCollection<Profile>
+		{
+			new Profile
+			{
+				Name = "Jack",
+				ProfileImage = "profile1.jpg",
+				NoPhotos = 25
+			},
+			                        
+			new Profile
+            {
+                Name = "Henry",
+                ProfileImage = "profile2.jpg",
+                NoPhotos = 13
+            },
+			                                   
+			new Profile
+            {
+                Name = "Maddy",
+                ProfileImage = "profile3.jpg",
+                NoPhotos = 8
+            },
+        };
+
+		GeneratedImages = new ObservableCollection<GeneratedImage>
+		{
+			new GeneratedImage
+			{
+				ImagePath = "dashboard1.jpg",
+				MainKeyword = "Castle",
+				Keywords = new List<string>
+				{
+					"Epic, lake, mountain, medieval, trees, grey sky"
+				}
+			},
+                        
+			new GeneratedImage
+            {
+                ImagePath = "dashboard2.jpg",
+                MainKeyword = "Mountain",
+                Keywords = new List<string>
+                {
+                    "Landscape, photorealistic, dawn, mountains, blue sky"
+                }
+            },
+		                        
+			new GeneratedImage
+            {
+                ImagePath = "dashboard3.jpg",
+                MainKeyword = "Robot",
+                Keywords = new List<string>
+                {
+                    "AI, rototic, human, light, metal, cartoon"
+                }
+            },
+
+        };
 	}
 }
