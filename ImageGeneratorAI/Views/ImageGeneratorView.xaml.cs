d4f2f18eab730e858bb01a34fc0999590902dade
Alexandr Cocier <Limos12345@outlook.com>
2023-11-09 21:41:58 +0200

    Add Timer to AI Image Generation

--- ImageGeneratorAI/Views/ImageGeneratorView.xaml.cs
+++ ImageGeneratorAI/Views/ImageGeneratorView.xaml.cs
@@ -1,19 +1,49 @@
+using System.Diagnostics;
+
 namespace ImageGeneratorAI.Views;
 
 public partial class ImageGeneratorView : ContentPage
 {
+	Stopwatch watch = new Stopwatch();
 	public ImageGeneratorView()
 	{
 		InitializeComponent();
 	}
 
-    private void btnFinish_Clicked(object sender, EventArgs e)
+    protected override async void OnAppearing()
     {
-		StopGeneration();
+		await Task.Delay(TimeSpan.FromSeconds(2));
+		watch.Start();
+
+		var cts = new CancellationTokenSource();
+
+		using(var timer = new PeriodicTimer(TimeSpan.FromSeconds(1)))
+		{
+			try
+			{
+				var counter = 0;
+				while (await timer.WaitForNextTickAsync(cts.Token))
+				{
+					if(counter == 5)
+					{
+						cts.Cancel();
+					}
+
+					var seconds = watch.Elapsed.Seconds;
+					lblTimer.Text = seconds.ToString();
+					counter++;
+				}
+			}
+			catch (TaskCanceledException)
+			{
+				await StopGeneration();
+			}
+		}
     }
 
-	private async void StopGeneration()
+	private async Task StopGeneration()
 	{
+		watch.Stop();
 		await Task.Delay(2000);
 
 		lottie.IsAnimationEnabled = false;
@@ -32,4 +62,9 @@ public partial class ImageGeneratorView : ContentPage
 		await btnFinish.ScaleTo(1, 1000);
 
 	}
+
+    private void btnFinish_Clicked(object sender, EventArgs e)
+    {
+		Shell.Current.GoToAsync(nameof(DashboardView));
+    }
 }
